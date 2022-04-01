de1fe517ddfc4281ccff32c946f5a069e74e9b7a
Jan Solar <jan.solar@gmail.com>
2022-04-02 00:43:13 +0200

    First iteration - extracts the tokens, but fails to extract composite tokens

--- ex.pl
+++ ex.pl
@@ -14,12 +14,20 @@ my @tradosSourceXML=<FILE>;
 close(FILE);
 
 my $lineCounter=0;
+my $tokenCounter=0;
 #while (<FILE>) {
 #while ($tradosSourceXML) {
 foreach (@tradosSourceXML) {
+#<source><g id="7">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</g></source>
+#<seg-source><g id="7"><mrk mtype="seg" mid="1">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</mrk></g></seg-source>
+#<target><g id="7"><mrk mtype="seg" mid="1">EMULZNÍ KONCENTRÁTY PESTICIDŮ OBSAHUJÍCÍ  ROPNÉ OLEJE A ZPŮSOBY POUŽITÍ</mrk></g></target>
+
+    while($_ =~ /<source>(<g id="\w+">)+(.*?)(<\/g>)+<\/source>(.*?)<target>(<g id="\w+">)+<mrk mtype="seg" mid="(\w+)">(.*?)<\/mrk>(<\/g>)+<\/target>/g) {
+            $tokenCounter++;
+            print "SOURCE ($tokenCounter): id=$6: $2\n";
+            print "TARGET ($tokenCounter): id=$6: $7\n";
+    }
     $lineCounter++;
-    #push @LineStack, $_ ;
-    print "Reading line: $lineCounter: $_\n";
 }
 print "Processed lines: $lineCounter\n";
 
