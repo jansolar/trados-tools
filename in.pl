f46956a7996ba12d2cbbccdc45bfe096d487a2dc
Jan Solar <jan.solar@gmail.com>
2022-04-02 12:44:53 +0200

    First iteration of IN file

--- in.pl
+++ in.pl
@@ -1,60 +1,75 @@
 #!/usr/bin/perl
-  
+
 # Modules used
 use strict;
 use warnings;
 
 my $sourceTradosFile=shift;
+my $targetTradosFile=$sourceTradosFile . ".NEW";
 my $sourceFile="trados-source.txt";
 my $targetFile="trados-target.txt";
 
 open (FILE, "<$sourceTradosFile");
-my @tradosSourceXML=<FILE>;
+my $tradosSourceXML=<FILE>;
 #chomp $LastAD;
 close(FILE);
 
-open(FHS, '>', $sourceFile) or die $!;
-open(FHT, '>', $targetFile) or die $!;
+open(FHS, $sourceFile) or die $!;
+my @sourceFileLines=<FHS>;
+close(FHS);
+
+open(FHT, $targetFile) or die $!;
+my @targetFileLines=<FHT>;
+close(FHT);
 
 my $lineCounter=0;
 my $tokenCounter=0;
 #while (<FILE>) {
 #while ($tradosSourceXML) {
-foreach (@tradosSourceXML) {
-#<source><g id="7">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</g></source>
-#<seg-source><g id="7"><mrk mtype="seg" mid="1">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</mrk></g></seg-source>
-#<target><g id="7"><mrk mtype="seg" mid="1">EMULZNÍ KONCENTRÁTY PESTICIDŮ OBSAHUJÍCÍ  ROPNÉ OLEJE A ZPŮSOBY POUŽITÍ</mrk></g></target>
 
-#    while($_ =~ /<source>(<g id="\w+">)+(.*?)(<\/g>)+<\/source>(.*?)<target>(<g id="\w+">)+<mrk mtype="seg" mid="(\w+)">(.*?)<\/mrk>(<\/g>)+<\/target>/g) {
-#            $tokenCounter++;
-#            print "SOURCE ($tokenCounter): id=$6: $2\n";
-#            print "TARGET ($tokenCounter): id=$6: $7\n";
-#    }
+foreach $targetLine (@targetFileLines) {
+    #<source><g id="7">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</g></source>
+    #<seg-source><g id="7"><mrk mtype="seg" mid="1">PESTICIDE EMULSION CONCENTRATES CONTAINING PETROLEUM DERIVED OILS AND METHODS OF USE</mrk></g></seg-source>
+    #<target><g id="7"><mrk mtype="seg" mid="1">EMULZNÍ KONCENTRÁTY PESTICIDŮ OBSAHUJÍCÍ  ROPNÉ OLEJE A ZPŮSOBY POUŽITÍ</mrk></g></target>
 
-    while($_ =~ /<target>(.*?)<\/target>/g) {
-        my $targetString=$1;
-        #print "--------------------------------------------------------------------------------\n";
-        #print "TARGET STRING: $targetString\n";
-        while ($targetString =~ /<mrk mtype="seg" mid="(\w+)">(.*?)<\/mrk>/g) {
-            $tokenCounter++;
-            #print "--------------------------------------------------------------------------------\n";
-            #print "S:id=$1: $2\n";
-            #print "---\n";
-            my $printSrting = "XID=" . $1 . ":" . $2 . "\n";
-            print $printSrting;
-            print FHS $printSrting;
-            print FHT $printSrting;
-        }
-    }
+
+    my $sourceLine = shift(@sourceFileLines);
 
     $lineCounter++;
+
+    chomp $targetLine;
+    chomp $sourceLine;
+
+    my $targetID;
+    my $targetText;
+    my $sourceID;
+    my $sourceText;
+
+    if ($targetLine =~ /^XID=(\w+):(.*)$/g) {
+        my $targetID = $1;
+        my $targetText = $2;
+    }
+    else {
+        print "CAN NOT PARSE TARGET LINE: $targetLine\n";
+        next;
+    }
+
+    if ($sourceLine =~ /^XID=(\w+):(.*)$/g) {
+        my $sourceID = $1;
+        my $sourceText = $2;
+    }
+    else {
+        print "CAN NOT PARSE SOURCE LINE: $sourceLine\n";
+        next;
+    }
+
+    die "Source vs Target file mismatch!!!\n" if ($sourceID != $targetID);
+
+    print "--------------------------------------------------------------------------------\n";
+    print "XID=$targetID    line=$lineCounter\n";
+    print "SOURCE=$sourceText\n";
+    print "TARGET=$targetText\n";
 }
-print "Processed lines:  $lineCounter\n";
-print "Processed tokens: $tokenCounter\n";
-
-close(FHS);
-close(FHT);
-
 
 
 
