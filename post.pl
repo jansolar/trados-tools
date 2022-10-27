b164a0a5ce5acbd047e1dec83f90369a3ddc2a0c
jansolar <58437999+jansolar@users.noreply.github.com>
2022-10-27 14:23:32 +0200

    Update post.pl

--- post.pl
+++ post.pl
@@ -1,4 +1,4 @@
-use strict;
+#use strict;
 use warnings;
 
 use HTTP::Request ();
@@ -7,14 +7,16 @@ use LWP::UserAgent;
 
 my $deeplAuthenticationKey = "a11c47a8-f648-b6bd-be7a-21d3f3255a60:fx";
 my $url = 'https://api-free.deepl.com/v2/translate';
-my $header = ['Authorization' => 'DeepL-Auth-Key ' . $deeplAuthenticationKey];
-my $data = {text => "Hello world", target_lang => 'CS'};
+#my $url = 'https://172.65.205.19/v2/translate';
 
-my $encoded_data = encode_json($data);
+my $header = ['Authorization' => 'DeepL-Auth-Key ' . $deeplAuthenticationKey, 'Content-Type' => 'application/x-www-form-urlencoded'];
+my $data = 'text=Hello world&target_lang=CS';
 
-print "Encoded data: $encoded_data\n";
+#my $encoded_data = encode_json($data);
 
-my $req = HTTP::Request->new('POST', $url, $header, $encoded_data);
+
+
+my $req = HTTP::Request->new('POST', $url, $header, $data);
 
 my $postMessage = $req->decoded_content;
 print "Post message: $postMessage\n";
